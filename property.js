83f72c6b6634c712ea5da597a6a07f912dc918ef
H-M-Masud <hmmasud163@gmail.com>
2022-02-25 21:29:32 +0600

    added property

---
+++ property.js
@@ -0,0 +1,18 @@
+const bottle ={color: 'yellow', hold: 'water', price: 50, isCleaned: true};
+
+// getting all property names
+const keys = Object.keys(bottle);
+// console.log(keys)
+
+// get all values
+const values = Object.values(bottle);
+// console.log(values)
+
+
+const pairs = Object.entries(bottle);
+// console.log(pairs)
+
+// Object.seal(bottle);
+// Object.freeze(bottle);
+delete bottle.isCleaned;
+console.log(bottle)
