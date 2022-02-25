35a8d8654c21ef0355b5b0059595d0b84382bf41
H-M-Masud <hmmasud163@gmail.com>
2022-02-25 20:57:52 +0600

    added object create

---
+++ object-create.js
@@ -0,0 +1,27 @@
+// using object literal
+const student = {name: "Sakib al hasan", job:"crickter"};
+
+// 2. constructor
+const person = new Object();
+
+// 3 
+// const human = Object.create(null)
+const human = Object.create(student);
+// console.log(human.job)
+
+// 4 class
+class People{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+}
+const peop = new People("manus", 12);
+// console.log(peop)
+
+// function
+function Manus(name){
+    this.name = name;
+}
+const man = new Manus('Kader');
+// console.log(man)
